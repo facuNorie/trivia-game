4a4bfd58f201f1be17ea3bbb91bad62707c7b8bb
Facu Noriega <noriegafacundo08@gmail.com>
2022-01-29 20:59:16 -0300

    Adding styles

--- src/components/Question/index.jsx
+++ src/components/Question/index.jsx
@@ -4,8 +4,15 @@ import { Text, Box } from '@chakra-ui/react'
 
 export default function Question({ text }) {
   return (
-    <Box bgColor='pink.400' color='white' boxShadow='xl' my='30px' mx={5} p='8'>
-      <Text fontSize='3xl' textAlign='center'>
+    <Box
+      bgColor='pink.400'
+      color='white'
+      boxShadow='xl'
+      my={{ base: '15px', md: '30px' }}
+      mx={5}
+      p={{ base: '4', md: '8' }}
+    >
+      <Text fontSize={{ base: 'xl', md: '3xl' }} textAlign='center'>
         {decode(text, { level: 'html5' })}
       </Text>
     </Box>
--- src/components/Start/index.jsx
+++ src/components/Start/index.jsx
@@ -1,10 +1,38 @@
 import React from 'react'
-import { Box, Button } from '@chakra-ui/react'
+import { Box, Button, Text } from '@chakra-ui/react'
 export default function Start({ setPlay }) {
   return (
-    <Box>
-      Trivia game
-      <Button onClick={() => setPlay(true)}>Play</Button>
+    <Box
+      h='100%'
+      display='flex'
+      justifyContent='space-around'
+      flexDirection='column'
+      alignItems='center'
+    >
+      <Box w='200px' textAlign='center'>
+        <Text
+          bgColor='pink.400'
+          color='white'
+          fontSize='3xl'
+          fontWeight='bold'
+          py='10px'
+        >
+          Trivia
+        </Text>
+        <Text color='white' fontSize='3xl' fontWeight='bold'>
+          Game
+        </Text>
+      </Box>
+
+      <Button
+        bgColor='pink.400'
+        color='white'
+        size='lg'
+        _hover={{ bg: 'pink.400' }}
+        onClick={() => setPlay(true)}
+      >
+        Play
+      </Button>
     </Box>
   )
 }
