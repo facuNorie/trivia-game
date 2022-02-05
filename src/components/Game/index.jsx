cb2dfd0442b2a5e863cd3886c30f9918cd28e207
Facu Noriega <noriegafacundo08@gmail.com>
2022-02-04 23:48:19 -0300

    Add record of score in localStorage

--- src/components/Game/index.jsx
+++ src/components/Game/index.jsx
@@ -1,19 +1,21 @@
 import { useState, useEffect } from 'react'
 import { useDisclosure, Box, Text, Progress, ScaleFade } from '@chakra-ui/react'
-
 import { getQuestions } from '../../services/triviaApi'
 import Question from '../Question'
 import Answer from '../Answer'
 import MessageResponse from '../MessageResponse'
 import DifficultyBadge from '../DifficultyBadge'
+import End from '../End'
 
-export default function Game() {
+export default function Game({ setPlay }) {
   const [allQuestions, setAllQuestion] = useState(null)
   const [question, setQuestion] = useState(null)
   const [answers, setAnswers] = useState([])
   const [gameProgress, setGameProgress] = useState(1)
   const [reveal, setReveal] = useState(false)
   const [message, setMessage] = useState('')
+  const [score, setScore] = useState(0)
+  const [record, setRecord] = useState(0)
 
   const { isOpen, onOpen, onClose } = useDisclosure()
 
@@ -42,43 +44,59 @@ export default function Game() {
 
   useEffect(() => {
     getAllQuestions()
+    localStorage.getItem('record') !== null &&
+      setRecord(localStorage.getItem('record'))
   }, [])
 
   return (
-    <Box
-      h='100%'
-      transition='all .3s'
-      style={{
-        boxShadow:
-          message !== ''
-            ? `0px 0px 80px 10px ${shadowEffectColor} inset`
-            : 'none',
-      }}
-    >
-      <Text fontWeight='bold' fontSize='xl' p={2} color='white'>
-        Question: {gameProgress}/10
-      </Text>
-      <ScaleFade initialScale={0.7} in={isOpen}>
-        <Question text={question !== null && question.question} />
-        <DifficultyBadge question={question} />
-        <MessageResponse title={message} />
-        <Answer
-          answers={answers}
-          reveal={reveal}
-          setReveal={setReveal}
-          gameProgress={gameProgress}
-          setGameProgress={setGameProgress}
-          generateQuestion={generateQuestion}
-          setMessage={setMessage}
+    <>
+      {gameProgress <= 10 ? (
+        <Box
+          h='100%'
+          transition='all .3s'
+          style={{
+            boxShadow:
+              message !== ''
+                ? `0px 0px 80px 10px ${shadowEffectColor} inset`
+                : 'none',
+          }}
+        >
+          <Text fontWeight='bold' fontSize='xl' p={2} color='white'>
+            Question: {gameProgress}/10
+          </Text>
+          <ScaleFade initialScale={0.7} in={isOpen}>
+            <Question text={question !== null && question?.question} />
+            <DifficultyBadge question={question} />
+            <MessageResponse title={message} />
+            <Answer
+              answers={answers}
+              reveal={reveal}
+              setReveal={setReveal}
+              gameProgress={gameProgress}
+              setGameProgress={setGameProgress}
+              generateQuestion={generateQuestion}
+              setMessage={setMessage}
+              score={score}
+              setScore={setScore}
+              difficulty={question?.difficulty}
+            />
+          </ScaleFade>
+          <Progress
+            hasStripe
+            isAnimated
+            value={gameProgress * 10}
+            max='100'
+            colorScheme='pink'
+          />
+        </Box>
+      ) : (
+        <End
+          score={score}
+          record={record}
+          setRecord={setRecord}
+          setPlay={setPlay}
         />
-      </ScaleFade>
-      <Progress
-        hasStripe
-        isAnimated
-        value={gameProgress * 10}
-        max='100'
-        colorScheme='pink'
-      />
-    </Box>
+      )}
+    </>
   )
 }
