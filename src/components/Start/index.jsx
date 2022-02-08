9ffb6555762674dbe91ec61eb36ad1dc64bf9ad5
Facu Noriega <noriegafacundo08@gmail.com>
2022-02-08 20:39:43 -0300

    animating start screen

--- src/components/Start/index.jsx
+++ src/components/Start/index.jsx
@@ -1,38 +1,86 @@
-import React from 'react'
-import { Box, Button, Text } from '@chakra-ui/react'
+import React, { useState, useEffect } from 'react'
+import {
+  Box,
+  Button,
+  Text,
+  SlideFade,
+  keyframes,
+  Spinner,
+  Link,
+} from '@chakra-ui/react'
+
+const bounce = keyframes`
+  from {transform: scale(1.2,1.2)}
+  to {transform: scale(1.4,1.4)}
+`
 export default function Start({ setPlay }) {
+  const [showText, setShowText] = useState(false)
+  const bounceAnimation = `${bounce} infinite .8s alternate`
+
+  useEffect(() => {
+    setTimeout(() => {
+      setShowText(true)
+    }, 2000)
+  }, [])
+
   return (
     <Box
       h='100%'
       display='flex'
-      justifyContent='space-around'
+      justifyContent={{ base: 'space-evenly', md: 'space-around' }}
       flexDirection='column'
       alignItems='center'
     >
-      <Box w='200px' textAlign='center'>
-        <Text
+      <Box w={{ base: '200px', md: '400px' }} textAlign='center'>
+        <SlideFade offsetY='50px' in={showText}>
+          <Text
+            bgColor='pink.400'
+            color='white'
+            fontSize={{ base: '4xl', md: '6xl' }}
+            fontWeight='bold'
+            py='10px'
+          >
+            Trivia
+          </Text>
+          <Text
+            color='white'
+            fontSize={{ base: '4xl', md: '6xl' }}
+            fontWeight='bold'
+          >
+            Game
+          </Text>
+        </SlideFade>
+      </Box>
+      <SlideFade in={!showText}>
+        <Spinner
+          size='xl'
+          color='pink.400'
+          thickness='4px'
+          speed='0.65s'
+          emptyColor='white'
+        />
+      </SlideFade>
+
+      <SlideFade offsetY='-100px' in={showText}>
+        <Button
           bgColor='pink.400'
           color='white'
-          fontSize='3xl'
-          fontWeight='bold'
-          py='10px'
+          size='lg'
+          _hover={{ bg: 'pink.400' }}
+          animation={bounceAnimation}
+          onClick={() => setPlay(true)}
         >
-          Trivia
-        </Text>
-        <Text color='white' fontSize='3xl' fontWeight='bold'>
-          Game
-        </Text>
-      </Box>
-
-      <Button
-        bgColor='pink.400'
-        color='white'
-        size='lg'
-        _hover={{ bg: 'pink.400' }}
-        onClick={() => setPlay(true)}
-      >
-        Play
-      </Button>
+          Play
+        </Button>
+        <Box position='absolute' bottom='0' right='0' color='white'>
+          <Link href='https://www.linkedin.com/in/facunorie/' isExternal>
+            <i className='bx bxl-linkedin' style={{ fontSize: '24px' }}></i>
+          </Link>
+          <Link href='https://twitter.com/noriedev' isExternal ml='10px'>
+            <i className='bx bxl-twitter' style={{ fontSize: '24px' }}></i>
+          </Link>
+        </Box>
+      </SlideFade>
     </Box>
   )
 }
